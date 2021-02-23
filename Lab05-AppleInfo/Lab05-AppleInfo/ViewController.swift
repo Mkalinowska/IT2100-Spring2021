ec890e1df014822762dabee24a3d4a6cc2260081
Marta Kalinowska <S01067501@acad.tri-c.edu>
2021-02-22 23:47:42 -0500

    Lab05-AppleInfo

---
+++ Lab05-AppleInfo/Lab05-AppleInfo/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Lab05-AppleInfo
+//
+//  Created by Marta Kalinowska on 2/22/21.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var webApple: WKWebView!
+    @IBOutlet weak var appleLink: UISegmentedControl!
+    
+    @IBAction func changeAppleLink(_ sender: Any) {
+     var appleURLString = ""
+     switch appleLink.selectedSegmentIndex{
+     case 0:
+        appleURLString = "https://en.wikipedia.org/wiki/Apple_Inc."
+     case 1:
+        appleURLString = "https://en.wikipedia.org/wiki/IOS"
+     case 2:
+        appleURLString = "https://en.wikipedia.org/wiki/Xcode"
+     case 3:
+        appleURLString = "https://en.wikipedia.org/wiki/Swift_(programming_language)"
+     default:
+        appleURLString = "https://en.wikipedia.org/wiki/Apple_Inc."
+     }
+     let myURL = URL(string: appleURLString)
+     let myRequest = URLRequest(url: myURL!)
+     webApple.load(myRequest)
+     }
+
+     override func viewDidLoad(){
+     super.viewDidLoad()
+     // Do any additional setup after loading the view, typically from a nib.
+     let myURL = URL(string:"https://en.wikipedia.org/wiki/Apple_Inc.")
+     let myRequest = URLRequest(url: myURL!)
+     webApple.load(myRequest)
+     webApple.allowsBackForwardNavigationGestures = true
+     }
+    
+     override func didReceiveMemoryWarning(){
+     super.didReceiveMemoryWarning()
+     // Dispose of any resources that can be recreated.
+     }
+}
