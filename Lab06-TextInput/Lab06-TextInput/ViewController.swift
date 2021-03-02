9a2d3809a64695208b0b23da9412c3d6139c2bb6
Marta Kalinowska <S01067501@acad.tri-c.edu>
2021-03-01 20:49:09 -0500

    Lab06-TextInput

---
+++ Lab06-TextInput/Lab06-TextInput/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Lab06-TextInput
+//
+//  Created by Marta Kalinowska on 3/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var field: UITextField!
+    @IBOutlet var button: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.returnKeyType = .done
+        field.autocapitalizationType = .words
+        field.autocorrectionType = .no
+        field.becomeFirstResponder()
+        field.delegate = self
+    }
+    
+    @IBAction func buttonTapped() {
+        field.resignFirstResponder()
+    }
+}
+    extension ViewController: UITextFieldDelegate {
+        
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            textField.resignFirstResponder()
+            
+            if let text = textField.text {
+            print("\(text)")
+            }
+            return true
+        }
+}
