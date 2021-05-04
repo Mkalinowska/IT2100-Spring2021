ff8b6531b6572fc1c1dfdfefb4f21997db68cfce
Mkalinowska <60045874+Mkalinowska@users.noreply.github.com>
2021-05-03 23:37:54 -0400

    Add files via upload

---
+++ Lab15-BindingIntro/Lab15-BindingIntro/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Lab15-BindingIntro.
+//
+//  Created by Marta Kalinowska on 5/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+   
+    @State var firstName = ""
+    
+    private func buttonPressed() {
+        print(firstName)
+    }
+        
+    var body: some View {
+
+        VStack {
+            Text(firstName)
+            
+            TextField($firstName, placeholder: Text("Enter name"))
+                .padding(12)
+            
+            Button(action: buttonPressed) {
+                Text("Press me!")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
